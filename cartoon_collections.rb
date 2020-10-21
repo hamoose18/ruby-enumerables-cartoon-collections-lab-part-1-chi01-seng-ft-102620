16b7f47ae2f63a24ec994ed1cfd25653468820cf
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-10-21 18:15:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,7 @@
 def greet_characters(array)
-  array.each {
-    |element| puts "Hello #{element}!"
-  }
+  
 end
 
 def list_dwarves(array)
- array.each_with_index do |element, index|
-    puts "##{index+1}.*#{element}"
-end
+ 
 end
